9e5fcb1d59ae5292c8e8fbaaece49498f884ee75
Israel Malagutti <israelmalagutti@gmail.com>
2024-01-08 13:44:39 -0300

    Types - Add path mapping

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            "@contexts": "./src/contexts",
+            "@dtos": "./src/dtos",
+            "@hooks": "./src/hooks",
+            "@services": "./src/services",
+            "@screens": "./src/screens",
+            "@storage": "./src/storage",
+            "@utils": "./src/utils",
+            "@routes": "./src/routes",
+          },
+        },
+      ],
+    ],
   };
 };
