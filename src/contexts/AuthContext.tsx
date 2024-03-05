dd3adfe5bad9f02a765b3ea41d8f6e70312bba31
Israel Malagutti <israelmalagutti@gmail.com>
2024-03-05 16:44:40 -0300

    Context - Create signIn function

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,9 +1,14 @@
 import { createContext, useState } from "react";
 
+import { api } from "@services/api";
+
 import { UserDTO } from "@dtos/index";
+import { saveUser } from "@storage/storageUser";
 
 type AuthContextType = {
   user: UserDTO;
+
+  signIn: (email: string, password: string) => Promise<void>;
 };
 
 type AuthProviderType = {
@@ -17,7 +22,24 @@ export const AuthContext = createContext({
 export function AuthProvider({ children }: AuthProviderType) {
   const [user, setUser] = useState({} as UserDTO);
 
+  const signIn = async (email: string, password: string) => {
+    try {
+      const { data } = await api.post("/sessions", { email, password });
+
+      if (data.user && data.token) {
+        await saveUser(data.user);
+        api.defaults.headers.common.Authorization = `Bearer ${data.token}`;
+
+        setUser(data.user);
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, signIn }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
